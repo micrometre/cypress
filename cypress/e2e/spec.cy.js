fc97f78bb8d0c16429e4e3d86915e4829f8c9498
Henok <henokwehibe@gmail.com>
2023-07-08 23:28:27 +0100

    adding more

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,5 +2,15 @@
   it('titles are correct', () => {
     const page = cy.visit('https://croydonwebservices.com/');
     page.get('title').should('have.text', 'Home | croydonwebservices.com')
+
   });
 })
+
+
+
+describe('testing brand icon ', () => {
+    it('loads home page', () => {
+      cy.visit('https://croydonwebservices.com/');
+      cy.get('.navbar-brand').click();
+    });
+  });
