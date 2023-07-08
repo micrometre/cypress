ec0260382cab851aa8e10766cc344eb502d1babd
Henok <henokwehibe@gmail.com>
2023-07-08 22:30:14 +0100

    sorting configs

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,4 +3,8 @@
     cy.visit('https://example.cypress.io')
     cy.contains('type')
   })
+  it('titles are correct', () => {
+    const page = cy.visit('https://croydonwebservices.com/');
+    page.get('title').should('have.text', 'croydonwebservices.com')
+  });
 })
