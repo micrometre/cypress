ce405fbf77fd6735580086e3b77e812635e0ed7b
Henok <henokwehibe@gmail.com>
2023-07-08 22:00:06 +0100

    sorting configs

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,12 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
 screenshotOnRunFailure: false,
+reporter: 'junit',
+reporterOptions: {
+  reportDir: 'results',
+  mochaFile: 'results/test-output.xml',
+  toConsole: false,
+},
 video: false,
   e2e: {
     setupNodeEvents(on, config) {
