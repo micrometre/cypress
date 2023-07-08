78d0652645ba49241f8783f49f84207140dba2ab
Henok <henokwehibe@gmail.com>
2023-07-08 22:56:15 +0100

    adding reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,19 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-screenshotOnRunFailure: false,
-reporter: 'junit',
-reporterOptions: {
-  reportDir: 'results',
-  mochaFile: 'results/test-output.xml',
-  toConsole: false,
-},
-video: false,
+  screenshotOnRunFailure: false,
+  video: false,
+  reporter: "cypress-multi-reporters",
+  reporterOptions: {
+    reporterEnabled: "mochawesome",
+    mochawesomeReporterOptions: {
+      reportDir: "cypress/reports/mocha",
+      quite: true,
+      overwrite: false,
+      html: false,
+      json: true
+    }
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,10 +1,6 @@
-  describe('My First Test', () => {
-  it('finds the content "type"', () => {
-    cy.visit('https://example.cypress.io')
-    cy.contains('type')
-  })
+  describe('Home page Test', () => {
   it('titles are correct', () => {
     const page = cy.visit('https://croydonwebservices.com/');
-    page.get('title').should('have.text', 'croydonwebservices.com')
+    page.get('title').should('have.text', 'Home | croydonwebservices.com')
   });
 })
