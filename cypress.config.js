bb04a03bcae6a65714ab84ecf4fd5757d6d850a0
Henok <henokwehibe@gmail.com>
2023-07-08 21:18:36 +0100

    sorting cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+screenshotOnRunFailure: false,
 video: false,
   e2e: {
     setupNodeEvents(on, config) {
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,6 @@
-describe('My First Test', () => {
-    it('Does not do much!', () => {
-      expect(true).to.equal(true)
-    })
+  describe('My First Test', () => {
+  it('finds the content "type"', () => {
+    cy.visit('https://example.cypress.io')
+    cy.contains('type')
   })
+})
