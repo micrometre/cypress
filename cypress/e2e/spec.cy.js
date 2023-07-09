7be4ac538974a328d0676088d6887cfd3f9e99e3
Henok <henokwehibe@gmail.com>
2023-07-09 12:03:58 +0100

    testing frontend

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,8 +3,6 @@ describe('The Home Page', () => {
     const page = cy.visit('/');
     page.get('title').should('have.text', 'Home | croydonwebservices.com')
   });
-
-
   it('finds the navbar-toggler collapsed ', () => {
     cy.visit('/')
     cy.get('.navbar-brand').click();
@@ -16,31 +14,14 @@ describe('The Home Page', () => {
 
 
 describe('Nav Menus', () => {
-
   context('iphone-5 resolution', () => {
-
     beforeEach(() => {
-
       cy.viewport('iphone-5')
-
     })
-
     it('displays mobile menu on click', () => {
-
-    cy.get('.navbar-brand')
-
-
-        .should('be.visible')
-
-        .find('i.hamburger')
-
-        .click()
-
-      cy.get('ul.slideout-menu').should('be.visible')
-
+    cy.visit('/')
+    cy.get('.navbar-toggler')
     })
-
   })
-
 })
 
